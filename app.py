931446f0481c98c33547022276707015e695179a
SADMarketing <106376060+SADMarketing@users.noreply.github.com>
2023-12-01 12:19:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,6 +69,77 @@ for i, question in enumerate(questions, start=1):
     reponses[question] = creer_question(f"{i}. {question}", 
                                         ["Tout à fait d'accord", "Plutôt d'accord", "Plutôt pas d'accord", "Pas du tout d'accord"],
                                         cle=f"question_{i}")
+import streamlit as st
+
+# Page header
+st.header("Enquête sur les habitudes d'achat et informations personnelles")
+
+# Sexe
+sexe = st.radio(
+    "Etes-vous ?",
+    ('Un homme', 'Une femme', 'Autre')
+)
+
+# Âge
+age = st.text_input("Quel est votre âge ?")
+
+# Profession
+profession = st.selectbox(
+    "Quelle est votre profession ?",
+    ('Agriculteur exploitant', 'Artisan, commerçant, chef d’entreprise', 'Cadre, profession libérale',
+     'Profession intermédiaire', 'Employé', 'Ouvrier', 'Retraité', 'Sans activité', 'Etudiant')
+)
+
+# Habitation
+habitation = st.radio(
+    "Habitez-vous ?",
+    ('En maison', 'En appartement')
+)
+
+# Code Postal
+code_postal = st.text_input("Quel est votre code postal ?")
+# Ici vous devez ajouter la logique pour recoder en rural / urbain / péri-urbain
+
+# Nombre de personnes dans le foyer
+nombre_personnes = st.selectbox(
+    "Combien de personnes vivent dans votre foyer ?",
+    ['1 personne (je vis seul.e)', '2 personnes', '3 personnes', '4 personnes', '5 personnes et +']
+)
+
+# Enfants dans le foyer
+enfants = st.radio(
+    "Avez-vous des enfants qui vivent avec vous ?",
+    ('Oui', 'Non')
+)
+
+# Si oui, combien d'enfants
+if enfants == 'Oui':
+    nb_enfants = st.number_input("Si oui, combien ?", min_value=1, max_value=10)
+
+    # Âges des enfants
+    ages_enfants = {}
+    for i in range(1, nb_enfants + 1):
+        ages_enfants[f'Enfant {i}'] = st.text_input(f"Quel âge a l'enfant {i} ?")
+
+# Voitures dans le foyer
+voitures = st.selectbox(
+    "Combien de voitures possédez-vous au sein du foyer ?",
+    ('Aucune', '1 voiture', '2 voitures', '3 voitures ou +')
+)
+
+# Voitures hybrides ou électriques
+voitures_hybrides_electriques = st.selectbox(
+    "Parmi les voitures de votre foyer, quel est le nombre de voitures hybrides ou électriques ?",
+    ('Aucune voiture hybride ou électrique', '1 voiture hybride ou électrique', '2 voitures hybrides ou électriques', '3 voitures hybrides ou électriques ou +')
+)
+
+# Revenus mensuels
+revenus = st.selectbox(
+    "Quels sont les revenus mensuels de votre foyer (nets, allocations comprises) ?",
+    ('Moins de 750€', '750-1150€', '1150-1500€', '1500-2300€', '2300-3000€', '3000-4500€', '4500-6100€', '6100-7600€', 'Plus de 7600€', 'Préfère ne pas répondre / ne sait pas')
+)
+
+
 
 # Boutons de navigation
 col1, col2 = st.columns(2)
