872eb8c2ee1382b9390dc70173e6c95c6a613049
SADMarketing <106376060+SADMarketing@users.noreply.github.com>
2023-12-04 18:59:57 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -19,4 +19,26 @@ def inference(responses : np.array):
   model = pickle.load()   # conditionnement choix de modèle question facultatif   // 
   pred = model.predict(responses) 
   return pred
+
+def preprocessing_cluster(df):
+    
+
+    var_profil = ['Sexe', 'Age', 'TYPOLOGIE', 'Age tranches', 'CSP', 'Habitat',
+           'Taille AA', 'Nb voitures du foyer',
+           'Nb voitures hybrides ou électriques', 'Revenus mensuels du foyer',
+           'CP', 'Insee', 'AA', 'Foyer', 'Enfants au foyer', 'Nb enfants foyer']
+
+    var_drop  = ['Sensibilité prix','Sensibilté au temps','Sensibilité ecolo','Sensibilité santé' , "Score prix","Sensible au temps" , 'Sensibilité ecolo.1', 'Sensibilité prix.1', 'Sensibilité santé.1']
+    var_drop1  = [ i+".1" for i in var_drop]
+    var_drop1 = []
+    train = df[df.columns.difference(var_profil+var_drop + var_drop1)]
+
+    #df = df.drop("ID" ,axis = 1 )
+
+    train____ = pd.concat([pd.get_dummies(df[[ 'Sexe', 'Age', 'Age tranches', 'CSP', 'Habitat',
+           'Nb voitures du foyer',
+           'Nb voitures hybrides ou électriques', 'Revenus mensuels du foyer',
+       'Foyer', 'Enfants au foyer', 'Nb enfants foyer']]).iloc[:,2:],train],axis = 1)
+
+    return train____
   
